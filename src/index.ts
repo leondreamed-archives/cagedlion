b1ce5f3528957a10e2105904ae80b3651d1d6efe
Leon Si <leonzalion@gmail.com>
2021-09-07 01:06:26 -0400

    added bounds

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import 'dotenv/config';
 
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 import fastify from 'fastify';
 import fastifyCors from 'fastify-cors';
 import fastifyRateLimit from 'fastify-rate-limit';
 import got from 'got';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault('America/Toronto');
 
 const app = fastify();
 app.register(fastifyCors, {
@@ -55,15 +62,48 @@ async function updateStatus() {
 	}
 }
 
+enum Weekday {
+	sunday = 0,
+	monday = 1,
+	tuesday = 2,
+	wednesday = 3,
+	thursday = 4,
+	friday = 5,
+	saturday = 6,
+}
+
+function createScheduleMessage() {
+	return `The lion isn't currently obliged to stay in his cage. He's scheduled to be live from 4:30pm to 9:30pm on weekdays, and 8:30am to 9:30pm on weekends. If you catch him roaming outside his cage by then, you'll receive compensation (in the form of a gift card code) for your trouble ;) `;
+}
+
 // Limit URL checks to once every minute
 app.get('/check', async (request, reply) => {
-	// If a minute has elapsed, refresh the status
+	// On weekends, don't check before 9am or after 9pm
+	const weekday = dayjs().day();
+	const hour = dayjs().hour();
+	const minute = dayjs().minute();
+	const minutes = hour * 60 + minute;
+
+	// Don't check on weekends before 8:30 AM or after 9:30 PM
+	if (weekday === Weekday.saturday || weekday === Weekday.sunday) {
+		if (minutes < 8 * 60 + 30 || minutes > 21 * 60 + 30) {
+			return reply.send(createScheduleMessage());
+		}
+	}
+	// Don't check on weekdays before 4:30 AM or after 9:30 PM
+	else {
+		if (minutes < 16 * 60 + 30 || minutes > 21 * 60 + 30) {
+			return reply.send(createScheduleMessage());
+		}
+	}
+
+	// If a minute has elapsed since the last check, recheck and update the status
 	if (Date.now() - lastTimestampChecked >= 60 * 1000) {
 		lastTimestampChecked = Date.now();
 		await updateStatus();
 	}
 
-	reply.send(createLastStatusMessage());
+	return reply.send(createLastStatusMessage());
 });
 
 const port = process.env.PORT || 3000;
